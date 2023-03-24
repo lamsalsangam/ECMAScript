807ea45e08bd631052baa3006c0b6ec1eee24a7f
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-03-24 08:05:45 +0545

    Redux toolkit usage

---
+++ Redux Toolkit/redux-info/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { CartIcons } from "../icons";
+import { useSelector } from "react-redux";
+import { store } from "../store";
+
+const Navbar = () => {
+  //   console.log(
+  //     useSelector((store) => {
+  //       console.log(store);
+  //     })
+  //   );
+  //   const amount = useSelector((store) => store.cart);
+  const { amount } = useSelector((store) => store.cart);
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcons />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
