8edf2125dd7b2a92d9ca5da574d5575ce444619f
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-04-02 07:28:54 +0545

    Fetched Data from TMDB API

---
+++ Next js/imdb/src/app/components/Results.jsx
@@ -0,0 +1,11 @@
+const Result = ({ results }) => {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>{result.original_title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Result;
