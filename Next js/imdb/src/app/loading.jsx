94466957a076bd9f53d23fc54fe055c053ef130c
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-04-07 07:58:22 +0545

    Added Loading Effect

---
+++ Next js/imdb/src/app/loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+      <img className="h-24" src="spinner.svg" alt="Loading...." />
+    </div>
+  );
+};
+export default Loading;
