0e004ac6066141c299f60c8b1680789669d781c9
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-04-08 18:23:50 +0545

    Added the Card Component

--- Next js/imdb/src/app/loading.jsx
+++ Next js/imdb/src/app/loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="flex justify-center">
-      <img className="h-24" src="spinner.svg" alt="Loading...." />
+      <img className="h-12" src="spinner.svg" alt="Loading...." />
     </div>
   );
 };
