1a9ce48755fb6260432d2946d31962a491969fd0
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-05-24 18:16:37 +0545

    All about the fetching

---
+++ Next js/app_router/src/app/users/page.tsx
@@ -0,0 +1,25 @@
+import getAllUsers from "@/lib/getAllUsers";
+import Link from "next/link";
+
+export default async function UsersPage() {
+  const userData: Promise<User[]> = getAllUsers();
+  const users = await userData;
+  const content = (
+    <section>
+      <h2>
+        <Link href={"/"}>Back to Home</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`users/${user.id}`}>{user.name}</Link>
+            </p>
+          </>
+        );
+      })}
+    </section>
+  );
+  return content;
+}
