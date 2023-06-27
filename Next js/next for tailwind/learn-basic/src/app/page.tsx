b8a2c1e8968b391532755dda31a48866c49c3dc2
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-27 09:37:16 +0545

    Starting the MERN Project

--- Next js/next for tailwind/learn-basic/src/app/page.tsx
+++ Next js/next for tailwind/learn-basic/src/app/page.tsx
@@ -3,12 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">Title 1</h1>
+      <p className="break-all">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse debitis
+        sint illum quibusdam eligendi praesentium, exercitationem doloribus
+        pariatur repellendus officia, excepturi, fuga neque unde nisi qui eum
+        blanditiis ab nulla!
+      </p>
+      {/* <h1 className="text-3xl">Title 1</h1>
       <h2 className="text-2xl">Title 2</h2>
       <h3 className="text-xl">Title 3</h3>
       <p className="text-base">A regular paragraph</p>
       <p className="text-sm">A description paragraph</p>
-      <p className="text-xs note">A little note</p>
+      <p className="text-xs note">A little note</p> */}
     </div>
   );
 }
---
+++ React JS/MERN-AUTH/Readme.md
@@ -0,0 +1,13 @@
+# MERN-AUTH
+
+---
+
+# Create the root folder
+
+- Initialize the package.json `npm init -y`
+- Type `npm i express dotenv mongoose  bcryptjs jsonwebtoken cookie-parser` for installing some server side dependency
+
+## Back-End
+
+- # Create the backend folder
+  -- Start typing the backend logic in the server.js file
