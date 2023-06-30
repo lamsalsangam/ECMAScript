c3a7af8a93801dc80ea2c9ae5ca00f9b6dbf1599
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-06-30 07:31:03 +0545

    Made Schema and connected it to the Database

--- React JS/MERN-AUTH/backend/server.js
+++ React JS/MERN-AUTH/backend/server.js
@@ -5,13 +5,20 @@ dotenv.config();
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
+import connectDB from "./config/db.js";
+
 const port = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
 
 // const port = 5000;
 
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
