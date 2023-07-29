5c99fe7e87d156c713e86091e0ab31b9ff3e820b
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-29 12:03:47 +0545

    Made Login In API route

--- Next js/auth-app/src/app/api/users/signup/route.ts
+++ Next js/auth-app/src/app/api/users/signup/route.ts
@@ -1,7 +1,7 @@
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
-var bcryptjs = require("bcryptjs");
+import bcryptjs from "bcryptjs";
 
 connect();
 
