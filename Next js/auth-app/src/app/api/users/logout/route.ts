9aad7207c9fd1fa16bc7b05914d0ce6a4bffa3a0
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-29 15:52:38 +0545

    Made the Logout API route

---
+++ Next js/auth-app/src/app/api/users/logout/route.ts
@@ -0,0 +1,17 @@
+//  In the basic way we have to clear out the token to logout from the app
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout successful",
+      success: true,
+    });
+    // clear out the token
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
