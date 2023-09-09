315c2236439bf06e26ff86536a6fe21aab5dd4e4
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-09-09 15:18:30 +0545

    Added the Routes and made the initial component page for it

--- React JS/BookStore-MERN/frontend/src/App.jsx
+++ React JS/BookStore-MERN/frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+
 function App() {
   return (
-    <>
-      <h1 className="bg-red-700">Hello World</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
   );
 }
 
