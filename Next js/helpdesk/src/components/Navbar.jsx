636d1a403c70cefb3205a2bfe3821b6872e0e5c6
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-09-21 14:35:10 +0545

    Added Navbar and Home page component and page

---
+++ Next js/helpdesk/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar({}) {
+  return (
+    <nav>
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
+    </nav>
+  );
+}
