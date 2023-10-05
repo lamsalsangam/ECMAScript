7c86b549f4cac12fe53d6f47d7118775be105a85
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-10-05 14:19:49 +0545

    Made the content md pages in routes (Finished)

--- Astro/learn/src/content/config.ts
+++ Astro/learn/src/content/config.ts
@@ -1,12 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-  schema: z.object({
-    author: z.string(),
-    date: z.string(),
-    image: z.string(),
-    title: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      categories: z.array(z.string()),
+      date: z.string(),
+      featured: z.boolean().default(false),
+      image: image(),
+      title: z.string(),
+    }),
 });
 
 export const collection = {
