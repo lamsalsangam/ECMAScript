0585993130ddc1a11bb8d30091fb9f5d86eed7e9
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-10-14 10:27:24 +0545

    Responsive Image Optimization

--- Next js/image-gallery/src/app/components/ImgContainer.tsx
+++ Next js/image-gallery/src/app/components/ImgContainer.tsx
@@ -7,13 +7,17 @@ type Props = {
 
 export default function ImgContainer({ photo }: Props) {
   return (
-    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
+    <div
+      key={photo.id}
+      className="h-64 relative bg-gray-200 rounded-xl overflow-hidden group"
+    >
       <Image
         src={photo.src.large}
         alt={photo.alt}
-        width={250}
-        height={250}
-      ></Image>
+        fill={true}
+        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        className="object-cover group-hover:opacity-75"
+      />
     </div>
   );
 }
