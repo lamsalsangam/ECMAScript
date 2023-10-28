0e44fe672e0ee6c75648e31e7a3da1228d6c4820
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-10-28 07:29:29 +0545

    Made the coloor choosing palette.

--- Next js/ecommerce-fs/src/app/product/[productId]/ProductDetails.tsx
+++ Next js/ecommerce-fs/src/app/product/[productId]/ProductDetails.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import SetColor from "@/components/products/SetColor";
 import { Rating } from "@mui/material";
 import { type } from "os";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 interface ProductDetailsProps {
   product: any;
@@ -40,11 +41,22 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
     quantity: 1,
     price: product.price,
   });
+
   const productRating =
     // here the acc "accumulator" is initialized to be 0 at the beginning.
     // data.reviews.reduce((acc, item) => item.rating + acc, 0) / data.reviews.length;
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg],
+  );
+
   return (
     <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
       <div>Images</div>
@@ -69,7 +81,11 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
           {product.inStock ? "In Stock" : "Out of stock"}
         </div>
         <Horizontal />
-        <div>color</div>
+        <SetColor
+          cartProduct={cartProduct}
+          images={product.images}
+          handleColorSelect={handleColorSelect}
+        />
         <Horizontal />
         <div>quantity</div>
         <Horizontal />
