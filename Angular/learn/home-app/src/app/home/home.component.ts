77309d3835bbda70dd018e2bdc19c1cbbb8a727a
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-11-14 14:29:02 +0545

    Made the component in the Angular

---
+++ Angular/learn/home-app/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  imports: [CommonModule, HousingLocationComponent],
+})
+export class HomeComponent {}
