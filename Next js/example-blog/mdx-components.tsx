59199b96f69ee6766a36013caa7dd67ff1ec9b55
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-12-01 16:58:42 +0545

    hydration is not yet done but work in progress

--- Next js/example-blog/mdx-components.tsx
+++ Next js/example-blog/mdx-components.tsx
@@ -17,7 +17,6 @@ import Image from "next/image";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
     ...components,
   };
 }
---
+++ Next js/example-blog/src/app/my-mdx-pages/page.tsx
@@ -0,0 +1,9 @@
+import Hello from "../../mdx-pages/hello.mdx";
+
+export default function Page() {
+  return (
+    <div className="mx-auto">
+      <Hello />
+    </div>
+  );
+}
