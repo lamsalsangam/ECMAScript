6bfdeb7ee7d6f46d853ba6c30cb412a85f695574
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2024-01-11 17:44:36 +0545

    Started the Day5 Maps AirBNB

---
+++ React Native/Dev/days/src/app/(days)/day5/index.tsx
@@ -0,0 +1,25 @@
+import { Button } from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import MarkdownDisplay from "@/components/day3/MarkdownDisplay";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const description = `
+# Animated splash screen
+`;
+
+const DayDetailsScreen = () => {
+  return (
+    <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: "Day 5: AirBNB Maps" }} />
+
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+
+      <Link href="/day5/airbnb" asChild>
+        <Button title="Go to the AirBNB" />
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default DayDetailsScreen;
