d9fa352a851da3fd36e51eb080371051511934d0
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2024-01-13 10:42:10 +0545

    Added the marker in the map

--- React Native/Dev/days/src/app/(days)/day5/airbnb.tsx
+++ React Native/Dev/days/src/app/(days)/day5/airbnb.tsx
@@ -1,12 +1,28 @@
 import { Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 export default function airbnb() {
   return (
     <View>
       <Stack.Screen options={{ headerShown: false }} />
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 33.835187690581655,
+          longitude: 132.77316811043863,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 33.835187690581655,
+            longitude: 132.77316811043863,
+          }}
+          title="Cyber Space | My Workplace"
+        />
+      </MapView>
     </View>
   );
 }
